f8f00066242cbc204b0763f3e06de0b21ecbd898
Swati <swati1372004@gmail.com>
2025-08-04 00:17:25 +0530

    Initial commit - YouTube Bookmark Chrome Extension

---
+++ background.js
@@ -0,0 +1,14 @@
+//how even the browser is going to know that we switches tab
+//we going to track the any recent change in the tabs and then identify that if that
+// is a youtube page
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+    if(tab.url && tab.url.includes("youtube.com/watch")) {
+        const queryParameters = tab.url.split("?")[1];
+        const urlParameters = new URLSearchParams(queryParameters);
+        
+        chrome.tabs.sendMessage(tabId, {
+            type: "NEW",
+            videoId: urlParameters.get("v")
+        });
+    }
+});
